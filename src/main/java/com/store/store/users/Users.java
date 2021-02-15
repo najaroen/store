a53715118aafc98c8e7c94434c9357cc737753b1
najaroen <najaroen_tik@live.com>
2021-02-15 16:58:23 +0700

    add docker-compose.yml

--- src/main/java/com/store/store/users/Users.java
+++ src/main/java/com/store/store/users/Users.java
@@ -1,6 +1,13 @@
 package com.store.store.users;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     private String name;
     private String email;
